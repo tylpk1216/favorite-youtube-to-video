da87e5370c40ba5b9e4d0f9a79b4e909b10f673c
Michael Kuo <tylpk1216@gmail.com>
2018-03-14 11:31:20 +0800

    set CURLOPT_HEADER = false

--- favorite-youtube-to-video.php
+++ favorite-youtube-to-video.php
@@ -24,7 +24,7 @@ function downloadVideo($id, $title, $url, $video)
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER , false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_TIMEOUT, 600);
-    curl_setopt($ch, CURLOPT_HEADER , true);
+    curl_setopt($ch, CURLOPT_HEADER , false);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION , true);
     curl_setopt($ch, CURLOPT_AUTOREFERER , true);
     curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36');
